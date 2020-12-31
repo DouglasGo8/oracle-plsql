08c3a5d2e6f7f067000b8e379746e62e8c810582
DouglasGo8 <sgtear@hotmail.com>
2020-12-31 11:21:05 -0300

    Records done

--- complete-plsql/ORA_HR_RECORDS.sql
+++ complete-plsql/ORA_HR_RECORDS.sql
@@ -2,16 +2,21 @@
 declare
     --r_emp employees%rowtype;
     type t_emp is record (f_name varchar2(50));
+    r_emp t_emp; -- mandatory
 begin
+
+
     --select * into r_emp from employees where employee_id = 101;
     --select first_name from employees where employee_id = 101;
     
-    t_emp := 'Douglas';
+    r_emp.f_name := 'Douglas';
     
     --dbms_output.put_line(r_emp.last_name || ' ' || r_emp.salary);
 
     --r_emp.salary := 2000;
 
     --dbms_output.put_line(r_emp.last_name || ' ' || r_emp.salary);
+    
+    dbms_output.put_line(r_emp.f_name);
 
 end;
